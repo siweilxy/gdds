4c39ad8c2534e1f1fa9f317c24341db3e4816ad8
siweilxy <siweilxy@163.com>
2019-12-02 14:17:26 +0800

    add pubcfg-detail

--- src/app/pubcfg-detail/pubcfg-detail.component.ts
+++ src/app/pubcfg-detail/pubcfg-detail.component.ts
@@ -1,15 +1,40 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { Location } from '@angular/common';
+
+import { Pubcfg }         from '../pubcfg';
+import { CltPubCfgService }  from '../clt-pub-cfg.service';
 
 @Component({
-  selector: 'app-pubcfg-detail',
+  selector: 'pubcfg-detail',
   templateUrl: './pubcfg-detail.component.html',
-  styleUrls: ['./pubcfg-detail.component.css']
+  styleUrls: [ './pubcfg-detail.component.css' ]
 })
 export class PubcfgDetailComponent implements OnInit {
+  @Input() pubcfg: Pubcfg;
 
-  constructor() { }
+  constructor(
+    private route: ActivatedRoute,
+    private cltPubCfgService: CltPubCfgService,
+    private location: Location
+  ) {}
 
-  ngOnInit() {
+  ngOnInit(): void {
+    this.getApinfo();
   }
 
+  getApinfo(): void {
+    const id = this.route.snapshot.paramMap.get('id')+"";
+    this.cltPubCfgService.getPubCfg(id)
+      .subscribe(pubcfg => this.pubcfg = pubcfg);
+  }
+
+  goBack(): void {
+    this.location.back();
+  }
+
+  // save(): void {
+  //   this.cltPubCfgService(this.pubcfg)
+  //     .subscribe(() => this.goBack());
+  // }
 }
